e0560e3893ecc29e2281ef29091d6b52e7ff7d8e
Rotty <cjhih43@gmail.com>
2022-09-24 21:05:14 +0900

    fix: nitro options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,10 @@ export default defineNuxtConfig({
       },
     ],
   ],
+  nitro: {
+    minify: true,
+    serveStatic: true, // TODO: if need CDN publish
+  },
   vite: {
     css: {
       preprocessorOptions: {
