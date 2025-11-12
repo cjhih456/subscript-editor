c771b9619ad234cc8cda351db95110f6b5eb2899
cjhih456 <cjhih43@gmail.com>
2025-11-12 20:29:40 +0900

    fix: clean code

--- components/mixins/Video/AudioWave.ts
+++ components/mixins/Video/AudioWave.ts
@@ -118,190 +118,185 @@ export default function AudioWave (
    * @returns waveform data
    */
   function dataByLevelDisplay () {
-    if (!data.dataByLevel[displayLevel.value]) {
-      const maxRound = Math.ceil(
-        data.waveformData.length / displayWaveBySec.value
+    if (data.dataByLevel[displayLevel.value]) { return data.dataByLevel[displayLevel.value] }
+    const maxRound = Math.ceil(
+      data.waveformData.length / displayWaveBySec.value
+    )
+    const buffer = []
+    for (let i = 0; i < maxRound; i++) {
+      const soundArea = data.waveformData.slice(
+        i * displayWaveBySec.value,
+        (i + 1) * displayWaveBySec.value
       )
-      const buffer = []
-      for (let i = 0; i < maxRound; i++) {
-        const soundArea = data.waveformData.slice(
-          i * displayWaveBySec.value,
-          (i + 1) * displayWaveBySec.value
-        )
-        const { maxArea, minArea } = soundArea.reduce(
-          (acc, cur, i) => {
-            if (i % 2 === 0) {
-              acc.maxArea = acc.maxArea < cur ? cur : acc.maxArea
-            } else {
-              acc.minArea = acc.minArea > cur ? cur : acc.minArea
-            }
-            return acc
-          },
-          { maxArea: 0, minArea: 0 }
-        )
-        buffer.push(maxArea, minArea)
-      }
-      data.dataByLevel[displayLevel.value] = buffer
-      return data.dataByLevel[displayLevel.value]
+      const { maxArea, minArea } = soundArea.reduce(
+        (acc, cur, i) => {
+          if (i % 2 === 0) {
+            acc.maxArea = acc.maxArea < cur ? cur : acc.maxArea
+          } else {
+            acc.minArea = acc.minArea > cur ? cur : acc.minArea
+          }
+          return acc
+        },
+        { maxArea: 0, minArea: 0 }
+      )
+      buffer.push(maxArea, minArea)
     }
+    data.dataByLevel[displayLevel.value] = buffer
     return data.dataByLevel[displayLevel.value]
+  }
+
+  function clearCanvas (canvas: HTMLCanvasElement, width: number, height: number) {
+    canvas.getContext('2d')?.clearRect(0, 0, width, height)
   }
   /**
    * Clear waveform canvas.
    */
   function clearWave () {
-    waveCanvas.value?.getContext('2d')?.clearRect(0, 0, widthSize.value, waveHeight * 2)
+    if (!waveCanvas.value) { return }
+    clearCanvas(waveCanvas.value, widthSize.value, waveHeight * 2)
   }
   /**
    * Clear timeline canvas.
    */
   function clearTimeline () {
-    timelineCanvas.value?.getContext('2d')?.clearRect(0, 0, widthSize.value, 20)
+    if (!timelineCanvas.value) { return }
+    clearCanvas(timelineCanvas.value, widthSize.value, 20)
   }
   /**
    * Draw waveform on wave Canvas
    * @param scroll scrolled position
    */
-  function drawWave (scroll: number) {
-    if (!waveCanvas.value) { return }
-    const canvas = waveCanvas.value.getContext('2d')
-    if (canvas) {
-      const skipPos = Math.floor(scroll * levelData.value.barPerSec) * 2
-      const visibleWidth = dataByLevelDisplay().slice(
-        skipPos,
-        skipPos + Math.round(widthSize.value / displayBarSize.value) * 2
+  function drawWave (canvas: HTMLCanvasElement | null | undefined, scroll: number) {
+    if (!canvas) { return }
+    const context = canvas.getContext('2d')
+    if (!context) { return }
+    const skipPos = Math.floor(scroll * levelData.value.barPerSec) * 2
+    const visibleWidth = dataByLevelDisplay().slice(
+      skipPos,
+      skipPos + Math.round(widthSize.value / displayBarSize.value) * 2
+    )
+    context.fillStyle = canvas.computedStyleMap().get('--wave-line-bar-color') as string
+    for (let pos = 0; pos < visibleWidth.length; pos += 2) {
+      context.fillRect(
+        (pos / 2) * displayBarSize.value,
+        waveHeight - visibleWidth[pos],
+        displayBarSize.value - 1,
+        visibleWidth[pos] - visibleWidth[pos + 1]
       )
-      canvas.fillStyle = getComputedStyle(waveCanvas.value).getPropertyValue('--wave-line-bar-color')
-      for (let pos = 0; pos < visibleWidth.length; pos += 2) {
-        canvas.fillRect(
-          (pos / 2) * displayBarSize.value,
-          waveHeight - visibleWidth[pos],
-          displayBarSize.value - 1,
-          visibleWidth[pos] - visibleWidth[pos + 1]
-        )
-      }
     }
   }
   /**
    * Generate time label on timeline Canvas
-   * @param c Canvas
-   * @param t time text
-   * @param x position
+   * @param canvas Canvas
+   * @param time time text
+   * @param position position
    */
-  function genTimeLabel (c: CanvasRenderingContext2D, t: string, x: number) {
-    if (!timelineCanvas.value) { return }
-    c.fillStyle = getComputedStyle(timelineCanvas.value).getPropertyValue('--time-line-font-color')
-    c.fillText(t, x + 3, 12)
-
+  function genTimeLabel (canvas: HTMLCanvasElement | null | undefined, time: string, position: number) {
+    if (!canvas) { return }
+    const context = canvas.getContext('2d')
+    if (!context) { return }
+    context.fillStyle = canvas.computedStyleMap().get('--time-line-font-color') as string
+    context.fillText(time, position + 3, 12)
     // time mask
-    c.strokeStyle = getComputedStyle(timelineCanvas.value).getPropertyValue('--time-line-mark-color')
-    c.beginPath()
-    c.moveTo(x, 0)
-    c.lineTo(x, 20)
-    c.stroke()
+    context.strokeStyle = canvas.computedStyleMap().get('--time-line-mark-color') as string
+    context.beginPath()
+    context.moveTo(position, 0)
+    context.lineTo(position, 20)
+    context.stroke()
   }
   /**
    * Draw timeline on timeline Canvas
    * @param scroll scrolled position
    */
-  function drawTimeline (scroll: number) {
-    const canvas = timelineCanvas.value?.getContext('2d')
-    if (canvas) {
-      canvas.font = "12px 'Roboto', 'Noto', sans-serif"
-      canvas.textAlign = 'left'
-      canvas.textBaseline = 'bottom'
-      const displaySpace = levelData.value.displayTerm * pixPerSec.value
-      const timeRound =
-        Math.ceil(widthSize.value / levelData.value.displayTerm / pixPerSec.value) + 1
-      const leftSpace = (scroll % levelData.value.displayTerm) * pixPerSec.value
-      const leftRound =
-        Math.floor(scroll / levelData.value.displayTerm) * levelData.value.displayTerm
+  function drawTimeline (canvas: HTMLCanvasElement, scroll: number) {
+    const context = canvas.getContext('2d')
+    if (!context) { return }
+    context.font = "12px 'Roboto', 'Noto', sans-serif"
+    context.textAlign = 'left'
+    context.textBaseline = 'bottom'
+    const displaySpace = levelData.value.displayTerm * pixPerSec.value
+    const timeRound =
+      Math.ceil(widthSize.value / levelData.value.displayTerm / pixPerSec.value) + 1
+    const leftSpace = (scroll % levelData.value.displayTerm) * pixPerSec.value
+    const leftRound =
+      Math.floor(scroll / levelData.value.displayTerm) * levelData.value.displayTerm
 
-      for (let i = 0; i < timeRound; i++) {
-        genTimeLabel(
-          canvas,
-          nuxt.$dayjs((i * levelData.value.displayTerm + leftRound) * 1000)
-            .utc()
-            .format(levelData.value.format),
-          Math.ceil(i * displaySpace - leftSpace)
-        )
-      }
+    for (let i = 0; i < timeRound; i++) {
+      genTimeLabel(
+        canvas,
+        nuxt.$dayjs((i * levelData.value.displayTerm + leftRound) * 1000)
+          .utc()
+          .format(levelData.value.format),
+        Math.ceil(i * displaySpace - leftSpace)
+      )
     }
   }
   /**
    * Generate Timeline and waveform
    */
   function genWave () {
-    if (
-      !import.meta.env.SSR &&
-      waveCanvas.value && timelineCanvas.value
-    ) {
-      requestAnimationFrame(() => {
-        if (
-          Array.isArray(data.waveformData) && data.waveformData.length &&
-          waveCanvas.value?.width === widthSize.value
-        ) {
-          clearWave()
-          drawWave(lazyScroll.value)
-        }
-        if (timelineCanvas.value) {
-          clearTimeline()
-          drawTimeline(lazyScroll.value)
-        }
-      })
-    }
+    if (import.meta.env.SSR) { return }
+    requestAnimationFrame(() => {
+      if (!waveCanvas.value || !timelineCanvas.value) { return }
+      if (
+        Array.isArray(data.waveformData) && data.waveformData.length &&
+        waveCanvas.value.width === widthSize.value
+      ) {
+        clearWave()
+        drawWave(waveCanvas.value, lazyScroll.value)
+      }
+      clearTimeline()
+      drawTimeline(timelineCanvas.value, lazyScroll.value)
+    })
   }
   /**
    * Detect the new file, generate waveform data, and extract a WAV file.
    */
-  watch(() => file.value, (n) => {
-    if (n) {
-      n.arrayBuffer().then(async (v: ArrayBuffer) => {
-        await nuxt.$ffmpeg.load()
-        await nuxt.$ffmpeg.writeFile(new Uint8Array(v), 'video')
-        await nuxt.$ffmpeg.transcodeWave('video', 'out.data', waveBySec).then((obj) => {
-          data.waveformData = obj.wave.map((v, idx) =>
-            Math.round(
-              (waveHeight * v) / Math.abs(obj.maxMinValue[idx % 2 ? 'min' : 'max'])
-            )
-          )
-          data.dataByLevel = {}
-          genWave()
-        })
-        // take duration of file
-        duration.value = await nuxt.$ffmpeg.takeMediaFileDuration()
-        // take audio file for whisper
-        const audioFile = await nuxt.$ffmpeg.transcodeAudio('video', 'out.wav')
-        const file = new File([audioFile], 'out.wav', { type: 'audio/wav' })
-        const formData = fetchData('post', {
-          audio_file: file
-        })
-        useCustomFetch('http://localhost:3000/whisper/asr', {
-          query: {
-            encode: false,
-            task: 'transcribe',
-            word_timestamps: true,
-            output: 'vtt'
-          },
-          cache: 'default',
-          keepalive: false,
-          duplex: 'half',
-          body: formData,
-          method: 'post'
-        }).then(async (result: any) => {
-          const cueList = await nuxt.$webVtt.parseSubtitle(result)
-          if (cueList) {
-            data.cueGeneratedData = cueList.cues
-          }
-        }).catch(() => {
-          alertMessage.value = 'Whisper are not working now.'
-          setTimeout(() => { alertMessage.value = undefined }, 2000)
-        })
-      })
-    }
+  watch(() => file.value, async (fileValue) => {
+    if (!fileValue) { return }
+    const arrayBuffer = await fileValue.arrayBuffer()
+    await nuxt.$ffmpeg.load()
+    await nuxt.$ffmpeg.writeFile(new Uint8Array(arrayBuffer), 'video')
+    await nuxt.$ffmpeg.transcodeWave('video', 'out.data', waveBySec).then((obj) => {
+      data.waveformData = obj.wave.map((v, idx) =>
+        Math.round(
+          (waveHeight * v) / Math.abs(obj.maxMinValue[idx % 2 ? 'min' : 'max'])
+        )
+      )
+      data.dataByLevel = {}
+    })
+    // take duration of file
+    duration.value = await nuxt.$ffmpeg.takeMediaFileDuration()
+    // take audio file for whisper
+    const audioFile = await nuxt.$ffmpeg.transcodeAudio('video', 'out.wav')
+    const file = new File([audioFile], 'out.wav', { type: 'audio/wav' })
+    const formData = fetchData('post', {
+      audio_file: file
+    })
+    useCustomFetch('http://localhost:3000/whisper/asr', {
+      query: {
+        encode: false,
+        task: 'transcribe',
+        word_timestamps: true,
+        output: 'vtt'
+      },
+      cache: 'default',
+      keepalive: false,
+      duplex: 'half',
+      body: formData,
+      method: 'post'
+    }).then(async (result: any) => {
+      const cueList = await nuxt.$webVtt.parseSubtitle(result)
+      if (cueList) {
+        data.cueGeneratedData = cueList.cues
+      }
+    }).catch(() => {
+      alertMessage.value = 'Whisper are not working now.'
+      setTimeout(() => { alertMessage.value = undefined }, 2000)
+    })
   })
-  watch(() => [lazyScroll.value, widthSize.value, displayLevel.value], (newVal, oldVal) => {
-    if (newVal[0] !== oldVal[0] || newVal[1] !== oldVal[1] || newVal[2] !== oldVal[2]) {
+  watch(() => [lazyScroll.value, widthSize.value, displayLevel.value, data.waveformData], (newVal, oldVal) => {
+    if (newVal[0] !== oldVal[0] || newVal[1] !== oldVal[1] || newVal[2] !== oldVal[2] || newVal[3] !== oldVal[3]) {
       genWave()
     }
   })
