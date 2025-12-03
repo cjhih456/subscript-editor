0f90dedfeb982399b891c40e9e01c9f258a8d91b
cjhih456 <cjhih43@gmail.com>
2025-12-03 16:27:02 +0900

    add: TimeBar component

---
+++ components/core/timeline/ui/TimeBar.tsx
@@ -0,0 +1,117 @@
+import { usePixPerSec, useScrollValue, useDisplayWidth } from '../../provider/SubtitleControllerProvider'
+
+export default defineNuxtComponent({
+  name: 'TimeBar',
+  props: {
+    timeBarHeight: {
+      type: Number,
+      default: 20
+    },
+    fontSize: {
+      type: Number,
+      default: 12
+    }
+  },
+  setup ({ timeBarHeight, fontSize }) {
+    const nuxt = useNuxtApp()
+    const canvas = useTemplateRef<HTMLCanvasElement>('canvas')
+    const pixPerSec = usePixPerSec()
+    const scrollValue = useScrollValue()
+
+    const displayWidth = useDisplayWidth()
+    const stepLevel = computed(() => {
+      if (pixPerSec.value >= 10000) {
+        return {
+          format: 'HH:mm:ss.SSS',
+          stepTime: 0.01
+        }
+      }
+      if (pixPerSec.value >= 5000) {
+        return {
+          format: 'HH:mm:ss.SSS',
+          stepTime: 0.05
+        }
+      }
+      if (pixPerSec.value >= 1000) {
+        return {
+          format: 'HH:mm:ss.SSS',
+          stepTime: 0.1
+        }
+      }
+      if (pixPerSec.value >= 200) {
+        return {
+          format: 'HH:mm:ss.SSS',
+          stepTime: 0.5
+        }
+      }
+      if (pixPerSec.value >= 100) {
+        return {
+          format: 'HH:mm:ss',
+          stepTime: 1
+        }
+      }
+      if (pixPerSec.value >= 20) {
+        return {
+          format: 'HH:mm:ss',
+          stepTime: 5
+        }
+      }
+      return {
+        format: 'HH:mm:ss',
+        stepTime: 10
+      }
+    })
+
+    watch([displayWidth, timeBarHeight, fontSize, scrollValue, pixPerSec], () => {
+      requestAnimationFrame(() => {
+        drawTimeBar()
+      })
+    })
+    onMounted(() => {
+      requestAnimationFrame(() => {
+        drawTimeBar()
+      })
+    })
+
+    function drawTimeBar () {
+      if (!canvas.value) { return }
+      const context = canvas.value.getContext('2d')
+      if (!context) { return }
+      const canvasWidth = canvas.value.offsetWidth
+      context.clearRect(0, 0, canvasWidth, timeBarHeight)
+      context.font = `${fontSize}px 'Roboto', 'Noto', sans-serif`
+      context.textAlign = 'left'
+      context.textBaseline = 'bottom'
+
+      let time = scrollValue.value / pixPerSec.value
+      let position = scrollValue.value % pixPerSec.value
+      while (position < canvasWidth) {
+        drawTimeLabel(nuxt.$dayjs((time) * 1000)
+          .utc()
+          .format(stepLevel.value.format), position)
+        time += stepLevel.value.stepTime
+        position += stepLevel.value.stepTime * pixPerSec.value
+      }
+    }
+
+    function drawTimeLabel (time: string, position: number) {
+      const context = canvas.value?.getContext('2d')
+      if (!context) { return }
+      context.fillStyle = 'black'
+      context.fillText(time, position + 3, (fontSize + timeBarHeight) / 2)
+      context.strokeStyle = 'black'
+      context.beginPath()
+      context.moveTo(position, 0)
+      context.lineTo(position, timeBarHeight)
+      context.stroke()
+    }
+  },
+  render () {
+    return <canvas
+      ref='canvas'
+      class="tw-absolute tw-left-0 tw-right-0 tw-w-full"
+      height={this.timeBarHeight}
+      width={(this.$refs.canvas as HTMLCanvasElement)?.offsetWidth || 0}
+    />
+  }
+})
