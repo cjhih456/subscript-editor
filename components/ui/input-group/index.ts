c475c191b3c11124d74d9c339ceeed492ea35e07
cjhih456 <cjhih43@gmail.com>
2025-12-09 22:14:34 +0900

    add: InputGroup VideoInput component

--- components/ui/input-group/index.ts
+++ components/ui/input-group/index.ts
@@ -9,6 +9,7 @@ export { default as InputGroupButton } from "./InputGroupButton.vue"
 export { default as InputGroupInput } from "./InputGroupInput.vue"
 export { default as InputGroupText } from "./InputGroupText.vue"
 export { default as InputGroupTextarea } from "./InputGroupTextarea.vue"
+export { default as InputGroupVideoInput } from "./InputGroupVideoInput.vue"
 
 export const inputGroupAddonVariants = cva(
   "text-muted-foreground flex h-auto cursor-text items-center justify-center gap-2 py-1.5 text-sm font-medium select-none [&>svg:not([class*='size-'])]:size-4 [&>kbd]:rounded-[calc(var(--radius)-5px)] group-data-[disabled=true]/input-group:opacity-50",
