61e9188db0a676dc935434496eef3674a9b7c637
cjhih456 <cjhih43@gmail.com>
2025-12-13 21:43:08 +0900

    fix: bar area hydration error

--- components/core/timeline/ui/BarArea.tsx
+++ components/core/timeline/ui/BarArea.tsx
@@ -47,15 +47,15 @@ export default defineNuxtComponent({
     }
   },
   render () {
-    return <div class="relative overflow-hidden bg-card text-card-foreground border-card-foreground">
-      <div ref="scrollArea" class="relative w-full overflow-x-scroll pb-2">
-        <div class="sticky top-0 left-0 right-0">
-          {this.$slots.canvas?.()}
+    return <ClientOnly>
+      <div class="relative overflow-hidden bg-card text-card-foreground border-card-foreground">
+        <div ref="scrollArea" class="relative w-full overflow-x-scroll pb-2">
+          <div class="sticky top-0 left-0 right-0">
+            {this.$slots.canvas?.()}
+          </div>
+          {this.$slots.cursor?.()}
+          {this.$slots.default?.()}
         </div>
-        {this.$slots.cursor?.()}
-        {this.$slots.default?.()}
-      </div>
-      <ClientOnly>
         {
           this.convertProgress > 0 ? (<>
             <div class="absolute top-0 bottom-0 left-0 right-0 bg-gray-500/30 z-10 flex items-center justify-center">
@@ -65,7 +65,7 @@ export default defineNuxtComponent({
             </div>
           </>) : (<></>)
         }
-      </ClientOnly>
-    </div>
+      </div>
+    </ClientOnly>
   }
 })
