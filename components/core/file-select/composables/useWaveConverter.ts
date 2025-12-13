21f6a92a8017f25de9fd4a818995d588c5c1667a
cjhih456 <cjhih43@gmail.com>
2025-12-13 21:24:10 +0900

    feat: implement wave conversion functionality and integrate progress display in BarArea

---
+++ components/core/file-select/composables/useWaveConverter.ts
@@ -0,0 +1,56 @@
+import { useAudioRate, useConvertProgress, useDuration, useVideoFile, useVideoFileObjectUrl, useWaveData, useWaveScaleValue } from "../../provider/SubtitleControllerProvider"
+import useFFmpeg from "./useFFmpeg"
+import type EventEmitter from "eventemitter3"
+
+export default function useWaveConverter () {
+  const nuxt = useNuxtApp()
+  const { clearVideoFileObjectUrl, setVideoFileObjectUrl } = useVideoFileObjectUrl()
+  const file = useVideoFile()
+  const duration = useDuration()
+  const audioRate = useAudioRate()
+  const waveScaleValue = useWaveScaleValue()
+  const waveData = useWaveData()
+  const convertProgress = useConvertProgress()
+  const emitter = ref<EventEmitter | null>(null)
+  const { loadFFmpeg, convertWave } = useFFmpeg()
+  watch(() => file.value, async (newFile) => {
+    if (!newFile) { return }
+    await loadFFmpeg()
+    emitter.value = await convertWave(newFile, audioRate.value)
+    clearVideoFileObjectUrl()
+    setVideoFileObjectUrl(newFile)
+    emitter.value.on('duration', (d: number) => {
+      duration.value = d
+    })
+    emitter.value.on('progress', (progress: number) => {
+      if (duration.value === 0) { return }
+      convertProgress.value = Math.round((progress / duration.value) * 100)
+    })
+    emitter.value.on('error', () => {
+      emitter.value?.off('duration')
+      emitter.value?.off('progress')
+      emitter.value?.off('done')
+      emitter.value?.off('error')
+      emitter.value = null
+      nuxt.$alert.show('Failed to convert wave data. Please try again.')
+      convertProgress.value = 0
+      waveScaleValue.value = 0
+      waveData.value = null
+      duration.value = 0
+    })
+    emitter.value.on('done', ({ wave, scaleValue }: { wave: SharedArrayBuffer, scaleValue: number }) => {
+      emitter.value?.off('duration')
+      emitter.value?.off('progress')
+      emitter.value?.off('done')
+      emitter.value?.off('error')
+      emitter.value = null
+      convertProgress.value = 0
+      waveScaleValue.value = scaleValue
+      waveData.value = wave
+    })
+  })
+
+  onBeforeUnmount(() => {
+    clearVideoFileObjectUrl()
+  })
+}
--- components/core/timeline/ui/BarArea.tsx
+++ components/core/timeline/ui/BarArea.tsx
@@ -1,11 +1,17 @@
-import { useScrollValue } from '../../provider/SubtitleControllerProvider'
+import { useConvertProgress, useScrollValue } from '../../provider/SubtitleControllerProvider'
 import { provideCursorController } from '../../provider/CursorControllerProvider'
+import { Progress } from '~/components/ui/progress'
+import { ClientOnly } from '#components'
+import useWaveConverter from '../../file-select/composables/useWaveConverter'
 
 export default defineNuxtComponent({
   name: 'BarArea',
   setup () {
     const scrollArea = useTemplateRef<HTMLDivElement>('scrollArea')
     const { value: scrollValue, left: scrollClientLeft } = useScrollValue()
+    const convertProgress = useConvertProgress()
+
+    useWaveConverter()
 
     provideCursorController(3)
 
@@ -36,9 +42,12 @@ export default defineNuxtComponent({
       window.removeEventListener('resize', windowResizeEvent, false)
       scrollArea.value?.removeEventListener('scroll', scrollEvent)
     })
+    return {
+      convertProgress
+    }
   },
   render () {
-    return <div class="overflow-hidden bg-card text-card-foreground border-card-foreground">
+    return <div class="relative overflow-hidden bg-card text-card-foreground border-card-foreground">
       <div ref="scrollArea" class="relative w-full overflow-x-scroll pb-2">
         <div class="sticky top-0 left-0 right-0">
           {this.$slots.canvas?.()}
@@ -46,6 +55,17 @@ export default defineNuxtComponent({
         {this.$slots.cursor?.()}
         {this.$slots.default?.()}
       </div>
+      <ClientOnly>
+        {
+          this.convertProgress > 0 ? (<>
+            <div class="absolute top-0 bottom-0 left-0 right-0 bg-gray-500/30 z-10 flex items-center justify-center">
+              <div class="flex-1 p-4">
+                <Progress class="shadow-xl shadow-primary" modelValue={this.convertProgress} max={100} />
+              </div>
+            </div>
+          </>) : (<></>)
+        }
+      </ClientOnly>
     </div>
   }
 })
