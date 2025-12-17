fc9272a69db027e4504c333f4cf8bb225449e1af
cjhih456 <cjhih43@gmail.com>
2025-12-17 14:47:32 +0900

    feat: implement subtitle file selection with confirmation dialog in SideMenu

--- components/core/side-menu/ui/SideMenu.tsx
+++ components/core/side-menu/ui/SideMenu.tsx
@@ -4,12 +4,19 @@ import { Button } from "~/components/ui/button"
 import useVideoFileSelect from "../composables/useVideoFileSelect"
 import useSaveCue from "../composables/useSaveCue"
 import { MenuIcon, Moon, Sun } from "lucide-vue-next"
+import useSubtitleFileSelect from "../composables/useSubtitleFileSelect"
+import { useCueStore } from "../../provider/SubtitleControllerProvider"
+import SubtitleFileSelectWarning from "./SubtitleFileSelectWarning"
 
 export default defineNuxtComponent({
   name: 'SideMenu',
   setup () {
     const sideMenuState = ref<boolean>(true)
+    const { loadCues } = useCueStore()
     const { open: openVideoFileSelect } = useVideoFileSelect()
+    const { open: openSubtitleFileSelect } = useSubtitleFileSelect({ onSuccess: (cues) => {
+      loadCues(cues)
+    } })
     const { saveCue, saveAble } = useSaveCue()
 
     const colorMode = useColorMode()
@@ -25,6 +32,11 @@ export default defineNuxtComponent({
       theme.value = !theme.value
     }
 
+    function openSubtitleFileSelectAction() {
+      openSubtitleFileSelect()
+      sideMenuState.value = false
+    }
+
     function openVideoFileSelectorAction() {
       openVideoFileSelect()
       sideMenuState.value = false
@@ -37,6 +49,7 @@ export default defineNuxtComponent({
 
     return {
       openVideoFileSelectorAction,
+      openSubtitleFileSelectAction,
       sideMenuState,
       saveAble,
       saveCueAction,
@@ -72,6 +85,21 @@ export default defineNuxtComponent({
             <Button onClick={() => this.openVideoFileSelectorAction()}>
               Open Video File
             </Button>
+            {this.saveAble ? (<SubtitleFileSelectWarning
+              title="Subtitle File Select Warning"
+              description="Are you sure you want to select a subtitle file?"
+              onConfirm={() => this.openSubtitleFileSelectAction()}
+              onCancel={() => {}}
+            >
+              {{
+                trigger: () => (<Button>
+                  Open Subtitle File
+                </Button>)
+              }}
+            </SubtitleFileSelectWarning>) : <Button onClick={() => this.openSubtitleFileSelectAction()}>
+              Open Subtitle File
+            </Button>}
+            
             <Button onClick={() => this.saveCueAction()} disabled={!this.saveAble}>
               Export Cues as VTT
             </Button>
---
+++ components/core/side-menu/ui/SubtitleFileSelectWarning.tsx
@@ -0,0 +1,43 @@
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogTitle, AlertDialogTrigger } from "~/components/ui/alert-dialog"
+
+export default defineNuxtComponent({
+  name: 'SubtitleFileSelectWarning',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    onConfirm: {
+      type: Function,
+    },
+    onCancel: {
+      type: Function,
+      required: true,
+    },
+  },
+  render () {
+    return <AlertDialog>
+      <AlertDialogTrigger asChild>
+        {this.$slots.trigger?.()}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogTitle>{this.title}</AlertDialogTitle>
+        <AlertDialogDescription>{this.description}</AlertDialogDescription>
+        <AlertDialogFooter>
+          {
+            this.onConfirm && <AlertDialogAction onClick={() => this.onConfirm?.()}>
+              Confirm
+            </AlertDialogAction>
+          }
+          <AlertDialogCancel onClick={() => this.onCancel()}>
+            Cancel
+          </AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  }
+})
