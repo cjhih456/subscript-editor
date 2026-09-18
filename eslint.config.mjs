93fdf74d1568ece25ddf9fd0ba07bff7f261a552
cjhih456 <cjhih43@gmail.com>
2026-09-18 18:51:40 +0900

    fix: packages & scripts & rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import { globalIgnores } from 'eslint/config'
 
 export default withNuxt(
   // Your custom configs here
+  [
+    globalIgnores([
+      './design-system/subscript-editor/scripts/**'
+    ]),
+    {
+      rules: {
+        'vue/require-default-prop': 'off'
+      },
+    }
+  ]
 )
